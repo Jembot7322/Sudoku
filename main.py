ba8bcaf15f03a4f6763bee24bdaf9b41438f242f
Jembot7322 <107512472+Jembot7322@users.noreply.github.com>
2022-06-25 23:29:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 from sudoku import Sudoku
 
+print("Welcome to the Super Sudoku Solver! ")
+menuchoice = 0
 s1 = Sudoku()
-s1.main_menu()
+while menuchoice != 5:
 
+    if menuchoice == 1:
+        newsize = int(input("What size grid? "))
+        s1.resetgrid(newsize)
+        s1.print_grid()
+    if menuchoice == 2:
+        newsize = int(input("What size grid? "))
+        s1.resetgrid(newsize)
+        s1.generate_sol(s1.grid)
+        s1.remove_numbers_from_grid()
+        print("done")
+        s1.print_grid()
+    if menuchoice == 3:
+        s1.generate_sol(s1.grid)
+        s1.print_grid()
 
-
-
-
+    print("1 - Generate Empty Puzzle\n"
+          "2 - Generate Partial Puzzle\n"
+          "3 - Solve Puzzle\n"
+          "4 - Exit")
+    menuchoice = int(input("Please input choice: "))
